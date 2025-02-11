c4a3fdb4c3e0fe435057308d66988e34577d2eca
Kushwanth9696 <158134940+Kushwanth9696@users.noreply.github.com>
2025-02-11 12:53:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RefExamples from './components/REfExamples';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Ref Examples App</h1>
+      <RefExamples />
+    </div>
+  );
+}
+
+export default App;
